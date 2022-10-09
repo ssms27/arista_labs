faa35bb4dca1c694739c015ca9f775919907eeea
ssms27 <ssms27@hotmail.com>
2022-10-08 23:00:08 -0400

    working example

--- arista.py
+++ arista.py
@@ -3,18 +3,31 @@ from jinja2 import Environment, FileSystemLoader
 from pathlib import Path
 
 ENV = Environment(loader=FileSystemLoader('.'))
-t = ENV.get_template("test.j2")
-
-location = ['DR', 'PROD']
+t = ENV.get_template("arista.j2")
 
 
 def get_ntp_config() -> object:
     global location
     global t
-    r = yaml.safe_load(Path("test.yml").read_text())
+    r = yaml.safe_load(Path("arista.yml").read_text())
 
     for i in location:
         print(t.render(name='name', ntp=r['Juniper'][i]['ntp']))
 
 
-get_ntp_config()
+def get_arista_config() -> object:
+    global t
+    r = yaml.safe_load(Path("arista.yaml").read_text())
+    print(t.render(hostname=r['arista']['tor']['hostname'],
+                   vlan=r['arista']['tor']['vlan'],
+                   vni=r['arista']['tor']['vni'],
+                   vteps=r['arista']['tor']['vteps'],
+                   asn=r['arista']['tor']['bgp']['asn'],
+                   router_id=r['arista']['tor']['bgp']['router_id'],
+                   neighbor=r['arista']['tor']['bgp']['neighbor'],
+                   remote_as=r['arista']['tor']['bgp']['remote_as'],
+                   networks=r['arista']['tor']['bgp']['networks']))
+
+
+# get_ntp_config()
+get_arista_config()
